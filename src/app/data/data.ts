1b56c0d273951fc3d57e6826de152fae026075ce
Ayush Prakash <itsnotmymailid140@gmail.com>
2025-01-20 09:10:34 +0000

    small change

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,40 +1,36 @@
 const projects = [
     {
-        id: 1,
+        id: 2,
         title: "Loki's-Fleet",
         description: "Ever wondered what if Loki from Marvel Cinematic universe owned a vehicle rental service? Well, here's a fictional website for the same.",
         url: "https://loki-fleet.vercel.app/",
-        img: "/go1.jpg",
         tags: [
             "ReactJs", "Framer Motion", "TailwindCSS",
         ]
     },
     {
-        id: 2,
+        id: 3,
         title: "Realm CRM",
         description: "App designed to create campaign, manage audience relations and send bulk notifications. Create segment from audience for targetted campaigning.",
         url: "https://realm-crm.vercel.app/",
-        img: "/go1.jpg",
         tags: [
             "NextJS", "Express", "RabbitMQ", "MongoDB",
         ]
     },
     {
-        id: 3,
-        title: "URL Shortener",
-        description: "URL shortener service built using Golang, Gin, MongoDB & Mongoose. It allows users to shorten long URLs into short ones.",
-        url: "https://github.com/prkshayush/go-urlshortener",
-        img: "/go2.jpg",
+        id: 4,
+        title: "Emplims",
+        description: "Employees information management system, in SpringBoot to create and manage employee data records for name, id, department, salary etc.",
+        url: "https://github.com/prkshayush/ems-records",
         tags: [
             "Java", "SpringBoot", "ReactJS", "SQL",
         ]
     },
     {
-        id: 4,
+        id: 1,
         title: "RemindRyt",
         description: "Productivity & Accountability app for procrastinators, make groups, add friends and create tasks. AI tracks the progress and maintains leaderbaord alongwith an encouraging message.",
         url: "https://github.com/prkshayush/remindryt",
-        img: "/",
         tags: [
             "Go", "NextJS", "Gemini AI", "PostgreSQL"
         ]
